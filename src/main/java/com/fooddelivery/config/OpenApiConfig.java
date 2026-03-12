c2dc266fca7d37125282eb1b1689072b7df5c35e
Nureles <almazbekuulunureles5@gmail.com>
2026-03-12 23:23:34 +0600

    feat: added menu item

--- src/main/java/com/fooddelivery/config/OpenApiConfig.java
+++ src/main/java/com/fooddelivery/config/OpenApiConfig.java
@@ -1,6 +1,5 @@
 package com.fooddelivery.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +10,11 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI foodDeliveryOpenAPI() {
-
         return new OpenAPI()
                 .info(new Info()
-                        .title("Food Delivery API")
-                        .description("Backend API for Food Delivery Platform")
-                        .version("1.0"));
+                        .title("Food Delivery Backend API")
+                        .version("1.0")
+                        .description("API для работы с пользователями, ресторанами, меню и заказами"));
     }
 }
 //http://localhost:8080/swagger-ui/index.html
