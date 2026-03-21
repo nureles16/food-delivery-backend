a0fd7d27b3a860d14136dae709a9aba93f5e3723
Nureles <almazbekuulunureles5@gmail.com>
2026-03-21 12:54:40 +0600

    fix: changes enum Order Status

--- src/main/java/com/fooddelivery/orders/entity/OrderStatus.java
+++ src/main/java/com/fooddelivery/orders/entity/OrderStatus.java
@@ -3,11 +3,13 @@ package com.fooddelivery.orders.entity;
 public enum OrderStatus {
 
     PENDING,
+    PAID,
     CONFIRMED,
     COOKING,
     READY,
+    COURIER_ASSIGNED,
     DELIVERING,
     DELIVERED,
-    CANCELLED
-
+    CANCELLED,
+    REFUNDED,
 }
